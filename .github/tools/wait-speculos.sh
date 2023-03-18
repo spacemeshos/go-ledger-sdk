27abf0cc0ea5e12ba0053a719b0c03f57b6523a1
Lane Rettig <lanerettig@gmail.com>
2023-03-18 15:42:47 +1300

    Update speculos wait msg
    Not sure why --sdk 1.6 prints "SDK version 4" but it does =\

--- .github/tools/wait-speculos.sh
+++ .github/tools/wait-speculos.sh
@@ -2,8 +2,7 @@
 
 counter=0
 while true; do
-  docker logs --tail=5 speculos
-  docker logs --tail=5 speculos 2>&1 | grep using\ SDK\ version\ 1.6
+  docker logs --tail=5 speculos 2>&1 | grep using\ SDK\ version\ 4\ on\ nanos
   if [ $? == 0 ]; then
     exit 0
   fi
